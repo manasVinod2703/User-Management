1d94267f20c935c8e227723be678538dca93d525
manasVinod2703 <110780616+manasVinod2703@users.noreply.github.com>
2022-09-20 09:18:07 +0530

    Updated the files

--- index.js
+++ index.js
@@ -16,9 +16,19 @@ const showError = (err)=>{
     setTimeout(()=>{
         error.innerHTML = "";
         error.classList.remove('show');
-    },2000);
-    console.log(error.classList)
+    },3000);
+    //console.log(error.classList)
     
+}
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+  {
+    return (true)
+  }
+    errorText = "Please Enter a valid email!"
+    return (false)
 }
 
 const validateInput = ()=>{
@@ -26,7 +36,29 @@ const validateInput = ()=>{
        errorText = "Please Fill all the values!"
        showError(errorText);
        console.log(errorText);
+       return false
     }
+
+    else if(!ValidateEmail()){
+        showError(errorText);
+        return false
+    }
+
+    else if(nameInput.value.length < 6){
+        errorText = "Please Enter a name with more than 6 characters!"
+        showError(errorText)
+        return false
+
+    }
+
+    else if(password.value.length < 8){
+        errorText = "Password must contain more than 8 characters!"
+        showError(errorText);
+        return false
+    }
+
+
+    return true;
   
 }
 
